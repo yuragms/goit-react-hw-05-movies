6f85be0f2868387c251c99bbcc6c24b51b3b058e
yuragms <yura.gms@gmail.com>
2021-10-01 14:14:24 +0300

    1

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { List, Item } from "Navigation.styled.jsx";
+import { List, Item } from "./Navigation.styled.jsx";
 
 const Navigation = () => (
   <List>
