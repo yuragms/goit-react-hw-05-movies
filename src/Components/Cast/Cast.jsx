979ccd14ae6b657ecc2c2b20eaa8ad6c949c5ace
yuragms <yura.gms@gmail.com>
2021-10-10 11:48:19 +0300

    3

--- src/Components/Cast/Cast.jsx
+++ src/Components/Cast/Cast.jsx
@@ -1,4 +1,4 @@
-import { useRouteMatch, useParams } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getCastInfoMovie } from "../../Servises/FetchMoviesApi.jsx";
 import { List, Item, Name, Character, Img } from "./Cast.styled";
@@ -6,6 +6,7 @@ import { List, Item, Name, Character, Img } from "./Cast.styled";
 const Cast = () => {
   const [movieId, setMovieId] = useState(null);
   const [cast, setCast] = useState(null);
+  const url = useRouteMatch();
 
   console.log(movieId);
   const imgBaseUrl = "https://image.tmdb.org/t/p/w92/";
